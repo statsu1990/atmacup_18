dc1c91994a1deaf5c406f5dfd8026bca77db5f49
statsu1990 <statsu1990@users.noreply.github.com>
2024-11-19 00:25:36 +0900

    信号機のbboxを描画した画像を作成するスクリプトを追加

--- atmacup_18/constants.py
+++ atmacup_18/constants.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+# data
 DATA_DIR = Path(".").joinpath("data")
 DATASET_DIR = DATA_DIR.joinpath("atmaCup#18_dataset")
 TR_FEATURES_CSV = DATASET_DIR.joinpath("train_features.csv")
@@ -8,3 +9,21 @@ IMAGES_DIR = DATASET_DIR.joinpath("images")
 TRAFFIC_LIGHTS_DIR = DATASET_DIR.joinpath("traffic_lights")
 
 TRAFFIC_LIGHTS_CSV = DATASET_DIR.joinpath("traffic_lights.csv")
+
+# image
+IMAGE_SIZE = (64, 128)
+
+
+# traffic light
+TRAFFIC_LIGHT_CLASS_INDEXES = {
+    "green": 0,
+    "red": 1,
+    "yellow": 2,
+    "empty": 3,
+    "right": 4,
+    "straight": 5,
+    "left": 6,
+    "other": 7,
+}
+
+TRAFFIC_LIGHT_BBOX_IMAGE_NAME = "traffic_light_bbox_image.npy"
